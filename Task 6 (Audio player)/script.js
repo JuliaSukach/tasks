2b3a5670d55595fbbb88ca6fa8742a499c6ff764
YuliaSukach <35064670+YuliaSukach@users.noreply.github.com>
2018-09-26 23:04:43 +0300

    add button volume(on/off) js; slider volume and button next song doesn't work

--- Task 6 (Audio player)/script.js
+++ Task 6 (Audio player)/script.js
@@ -1,224 +1,3 @@
-
-/*let showSongOne = function () {
-	console.log("Play song...");
-
-	let oneSong = new Audio('playlist/Kanye West - Famous.mp3');
-	oneSong.play();
-	
-	let music = document.getElementById('song-1');
-
-	let result = music.appendChild(oneSong);
-
-	let nameSong = document.querySelector("label.first");
-	
-	let broadcast = document.getElementById('name-song');
-	
-	broadcast.innerText = nameSong.innerText;
-
-	let stateSong = document.getElementById('state');
-	stateSong.innerHTML = 'Playing...';
-	
-	
-	let pausePlaylist = function () {
-		oneSong.pause();
-	}
-	
-	let pause = document.getElementById('button-start-pause');
-	pause.onclick = pausePlaylist;
-
-}
-
-document.getElementById('song-1').onclick = function (event) {
-	let what = event.target
-}
-
-
-let showSongTwo = function () {
-	console.log("Play song...");
-	let twoSong = new Audio('playlist/Kanye West & Kid Cudi – Reborn.mp3');
-	twoSong.autoplay = true;
-}
-
-let showSongThree = function () {
-	console.log("Play song...");
-	let threeSong = new Audio('playlist/SiR – Summer in November.mp3');
-	threeSong.autoplay = true;
-}
-
-let showSongFour = function () {
-	console.log("Play song...");
-	let fourSong = new Audio('playlist/SiR - War.mp3');
-	fourSong.autoplay = true;
-}
-
-let showSongFive = function () {
-	console.log("Play song...");
-	let fiveSong = new Audio('playlist/Kid Cudi - Man on the Moon.mp3');
-	fiveSong.autoplay = true;
-}
-
-let showSongSix = function () {
-	console.log("Play song...");
-	let sixSong = new Audio('playlist/Kid Cudi - Rose Golden.mp3');
-	sixSong.autoplay = true;
-}
-
-let showSongSeven = function () {
-	console.log("Play song...");
-	let sevenSong = new Audio('playlist/Saint Jhn – I Heard You Got Too Litt Last Night.mp3');
-	sevenSong.autoplay = true;
-}
-
-let showSongEight = function () {
-	console.log("Play song...");
-	let eightSong = new Audio('playlist/HONNE- I Got You.mp3');
-	eightSong.autoplay = true;
-}
-
-let showSongNine = function () {
-	console.log("Play song...");
-	let nineSong = new Audio('playlist/Jaden Smith - Icon.mp3');
-	nineSong.autoplay = true;
-}
-
-let showSongTen = function () {
-	console.log("Play song...");
-	let tenSong = new Audio('playlist/Jaden Smith - Ghost.mp3');
-	tenSong.autoplay = true;
-}
-
-let playSongOne = document.getElementById('song-1');
-let playSongTwo = document.getElementById('song-2');
-let playSongThree = document.getElementById('song-3');
-let playSongFour = document.getElementById('song-4');
-let playSongFive = document.getElementById('song-5');
-let playSongSix = document.getElementById('song-6');
-let playSongSeven = document.getElementById('song-7');
-let playSongEight = document.getElementById('song-8');
-let playSongNine = document.getElementById('song-9');
-let playSongTen = document.getElementById('song-10');
-
-playSongOne.addEventListener("click", showSongOne);
-playSongTwo.addEventListener("click", showSongTwo);
-playSongThree.addEventListener("click", showSongThree);
-playSongFour.addEventListener("click", showSongFour);
-playSongFive.addEventListener("click", showSongFive);
-playSongSix.addEventListener("click", showSongSix);
-playSongSeven.addEventListener("click", showSongSeven);
-playSongEight.addEventListener("click", showSongEight);
-playSongNine.addEventListener("click", showSongNine);
-playSongTen.addEventListener("click", showSongTen);
-*/
-
-/////////////////////////////////
-
-
-/*let oneSong = new Audio('playlist/Kanye West - Famous.mp3');
-let twoSong = new Audio('playlist/Kanye West & Kid Cudi – Reborn.mp3');
-let threeSong = new Audio('playlist/SiR – Summer in November.mp3');
-let fourSong = new Audio('playlist/SiR - War.mp3');
-let fiveSong = new Audio('playlist/Kid Cudi - Man on the Moon.mp3');
-let sixSong = new Audio('playlist/Kid Cudi - Rose Golden.mp3');
-let sevenSong = new Audio('playlist/Saint Jhn – I Heard You Got Too Litt Last Night.mp3');
-let eightSong = new Audio('playlist/HONNE- I Got You.mp3');
-let nineSong = new Audio('playlist/Jaden Smith - Icon.mp3');
-let tenSong = new Audio('playlist/Jaden Smith - Ghost.mp3');
-
-let nameSong = document.querySelector("label.first");	
-let broadcast = document.getElementById('name-song');	
-broadcast.innerText = nameSong.innerText;
-
-let state = document.getElementById('state');
-state.innerText = 'Paused...';
-
-let turnOnSong = function () {
-	oneSong.play();
-	
-}
-
-let start_button = document.getElementById('button-start-pause');
-start_button.onclick = turnOnSong;
-
-
-let playSong = document.getElementById('playlist');
-
-playSong.onclick = function (event) {
-	console.log(event);
-	if(event.target.className == 'first') {
-		oneSong.play();
-		let label = document.querySelector('.first');
-		label.appendChild(oneSong);
-		console.log(label);	
-	}
-
-	if(event.target.className == 'second') {
-		twoSong.play();
-		let label = document.querySelector('.second');
-		label.appendChild(twoSong);
-		console.log(label);	
-	}
-
-	if(event.target.className == 'third') {
-		threeSong.play();
-		let label = document.querySelector('.third');
-		label.appendChild(threeSong);
-		console.log(label);	
-	}
-
-	if(event.target.className == 'fourth') {
-		fourSong.play();
-		let label = document.querySelector('.fourth');
-		label.appendChild(fourSong);
-		console.log(label);	
-	}
-
-	if(event.target.className == 'fifth') {
-		fiveSong.play();
-		let label = document.querySelector('.fifth');
-		label.appendChild(fiveSong);
-		console.log(label);	
-	}
-
-	if(event.target.className == 'sixth') {
-		sixSong.play();
-		let label = document.querySelector('.sixth');
-		label.appendChild(sixSong);
-		console.log(label);	
-	}
-
-	if(event.target.className == 'seventh') {
-		sevenSong.play();
-		let label = document.querySelector('.seventh');
-		label.appendChild(sevenSong);
-		console.log(label);	
-	}
-
-	if(event.target.className == 'eighth') {
-		eightSong.play();
-		let label = document.querySelector('.eighth');
-		label.appendChild(eightSong);
-		console.log(label);	
-	}
-
-	if(event.target.className == 'ninth') {
-		nineSong.play();
-		let label = document.querySelector('.ninth');
-		label.appendChild(nineSong);
-		console.log(label);	
-	}
-
-	if(event.target.className == 'tenth') {
-		tenSong.play();
-		let label = document.querySelector('.tenth');
-		label.appendChild(tenSong);
-		console.log(label);	
-	}
-
-	broadcast.innerText = event.target.innerText;
-	state.innerText = 'Playing...';
-};
-*/
-//listing
 let songs = [
 	'Kanye West – Famous.mp3',
 	'Kanye West & Kid Cudi – Reborn.mp3',
@@ -231,23 +10,29 @@ let songs = [
 	'Jaden Smith – Ghost.mp3'
 ]
 
-let song = new Audio (); //create <audio>
+let song = new Audio ();
+song.id = 'track'; //create <audio>
+console.log(song);
 let currentSong = 0;
 
 let state = document.getElementById('state'); // span with value playing or paused
 let broadcast = document.getElementById('name-song'); //label with name song
 let songSlider = document.getElementById('song-slider'); // input with time
+let volumeSlider = document.getElementById('volumeControl'); // input with time
 let playOrPauseButton = document.getElementById('button-start-pause'); //button start/pause
 let buttonReload = document.getElementById('button-reload'); //button reload
+let buttonNext = document.getElementById('button-next');// button next track
+let buttonVolume = document.getElementById('button-volume');
 let playSong = document.getElementById('playlist'); // ol 
 let music = document.getElementById('music'); // li with song
 let currentTime = document.getElementById('current-time');
 let duration = document.getElementById('duration');
+let audio = document.getElementById('track');
 
 window.onload = loadSong;
 
 function loadSong () {
-	console.log('playing...')
+	console.log('playing...');
 	song.src = 'playlist/' + songs[currentSong];
 	let rename = songs[currentSong].split('.');
 	broadcast.innerText = rename[0];
@@ -269,9 +54,6 @@ let updateSongSlider = function () {
 	console.log(c);
 	songSlider.value = c;
 	currentTime.textContent = convertTime(c);
-	/*if(song.ended){
-		next();
-	}*/
 }
 
 function convertTime (time) {
@@ -291,15 +73,15 @@ function showDuration () {
 }
 
 function playOrPauseSong () {
-	let img = document.getElementById('button-start-pause');
+	song.volume = 0.5;
 	if(song.paused) {
 		song.play();
-		img.src = "icons/pause.png";
+		playOrPauseButton.src = "icons/pause.png";
 		state.innerText = 'Playing...';
 	}
 	else {
 		song.pause();
-		img.src = "icons/play.png";
+		playOrPauseButton.src = "icons/play.png";
         state.innerText = 'Paused...';
 	}
 }
@@ -311,9 +93,34 @@ let playAgainSong = function () {
 	song.play();
 }
 
+/*let nextSong = function () {
+	console.log('Next song...');
+}*/
 
+let volumeSong = function (amount) {
+	console.log('Change volume...');
+    song.volume == volumeSlider.value; //it doesn't work
+}
+
+let muteSong = function () {
+	if (song.volume != 0) {
+		song.volume = 0;
+		buttonVolume.src = "icons/mute.png";
+		volumeSlider.value = 0;
+	}
+	else {
+		song.volume = 0.5;
+		buttonVolume.src = "icons/volume.png";
+		volumeSlider.value = 0.5;
+	}
+	
+}
+
+volumeSlider.oninput = volumeSong;
+//buttonNext.onclick = nextSong;
 playSong.onclick = startSong;
 buttonReload.onclick = playAgainSong;
+buttonVolume.onclick = muteSong;
 playOrPauseButton.onclick = playOrPauseSong;
 
 
